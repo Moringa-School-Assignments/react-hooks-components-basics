2bbbce85058c6d9b69d746032db013b64c175588
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-24 14:10:42 +0300

    complete assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Like from "./Like";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Like />
     </div>
   );
 }
---
+++ src/Like.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// class Likes extends Component {
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         );
+//     }
+// }
+
+function Like(){
+    return <div>I like this person</div>;
+}
+
+export default Like;
